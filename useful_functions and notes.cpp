573fb18d850fe78c54677ee1605dd732562eaaff
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2020-03-28 02:55:38 +0530

    Create useful_functions and notes.cpp

---
+++ useful_functions and notes.cpp
@@ -0,0 +1,12 @@
+concepts:
+getline(cin,str); //for inputting line
+string(s) //for typecasting character to string
+(char *)str.c_str() //for converting string to character array
+swap(obj1,obj2) //obj of any data type
+pair<trpe1,typr2> name[array ele]; name[i].first=__; name[i].second=__;
+
+
+
+important functions:
+strtok(chr,"delim"); //69919
+sort(array+0(from),array+5(to),comparator); //69919
