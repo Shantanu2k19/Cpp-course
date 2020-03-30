2bf6f641d6341a2b9d6cb9e9048f9d2ed6f594e0
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2020-03-30 21:49:15 +0530

    pair, strtok, getline, typecasting

--- useful_functions and notes.cpp
+++ useful_functions and notes.cpp
@@ -2,11 +2,18 @@ concepts:
 getline(cin,str); //for inputting line
 string(s) //for typecasting character to string
 (char *)str.c_str() //for converting string to character array
-swap(obj1,obj2) //obj of any data type
-pair<trpe1,typr2> name[array ele]; name[i].first=__; name[i].second=__;
+ 
+//pairs
+pair<int,string> p1=make_pair(100,"shan"); //for inserting in i d pair, kinda
+pair<trpe1,typr2> name[array ele]; name[i].first=__; name[i].second=__;  //2d pair
+pair<  pair<int,int> , string > p3; //pair of pair
+p3.first.first=1;
+p3.first.second=2;
+p3.second="car";
+
 
 
 
 important functions:
+
 strtok(chr,"delim"); //69919
-sort(array+0(from),array+5(to),comparator); //69919
