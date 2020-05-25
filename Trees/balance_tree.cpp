bbe385f37d6a2dee805650e52ecb41837255c872
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2020-05-26 05:29:37 +0530

    check if the tree is balanced or not
    a tree is balanced if the difference of height/depth of any two nodes doesn't differ by more than 1

---
+++ Trees/balance_tree.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d)
+    {
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node* constructTree()  //returns address of root node
+{
+    int d;
+    cin>>d;
+    if(d==-1)   // data of form : 3 -1 -1 means, 3 root, child none
+    { return NULL; }  
+
+    node* root = new node(d);
+    root->left = constructTree();
+    root->right = constructTree();
+    return root; 
+}
+
+node* insertInTree()
+{
+    int data;
+    cin>>data;
+
+    if(data==-1)
+    { return NULL; }
+    node* root = new node(data);
+    root->left = insertInTree();
+    root->right = insertInTree();
+    return root;
+}
+
+/*
+            1
+      2          5
+    3   4      6   7
+                   2   3
+*/
+
+class HBpair{
+    public: 
+    int height;
+    bool balance;
+};
+
+HBpair isHeightBalanced(node *root){
+    HBpair p;
+    if(root == NULL){
+        p.height = 0;
+        p.balance = false; 
+        return p;
+    }
+
+    HBpair left = isHeightBalanced(root->left);
+    HBpair right = isHeightBalanced(root->right);
+
+    p.height = max(left.height, right.height) + 1;
+
+    if(abs(left.height - right.height) <=1 && left.balance && right.balance){
+        p.balance = true;
+    }
+    else{
+        p.balance = false;
+    }
+    return p;
+}
+
+int main() 
+{
+    node* lol= constructTree();
+   if(isHeightBalanced){
+       cout<<"yes";
+   }
+   else{
+       cout<<"no";
+   }
+}
